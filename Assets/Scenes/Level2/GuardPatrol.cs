1f8be2f99809ae15373d7f7c10f35484992d1bc7
ammarmaulana4321 <ammarmaulana4321@gmail.com>
2026-07-14 00:59:29 +0700

    WIP: progres pengerjaan level 2 belum selesai

---
+++ Assets/Scenes/Level2/GuardPatrol.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class GuardPatrol : MonoBehaviour
+{
+    public float speed = 2f;
+    public float leftLimit = -3f;
+    public float rightLimit = 3f;
+
+    private bool movingRight = true;
+
+    void Update()
+    {
+        if (movingRight)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+            if (transform.position.x >= rightLimit)
+            {
+                movingRight = false;
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+        }
+        else
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+            if (transform.position.x <= leftLimit)
+            {
+                movingRight = true;
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+        }
+    }
+}
